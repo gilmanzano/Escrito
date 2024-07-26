7f4f08d962c628c51981c4f9dc3836030acaa526
gilmanzano <fede.gus.guida@gmail.com>
2024-07-26 12:45:06 -0300

    alta en el controller

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Personas;
+class PersonaController extends Controller
+{
+    public function listar(){
+        $personas = Personas::all();
+        return view('listado', compact('personas'));
+    }
+    public function alta(Request $request){
+
+        $persona  = new Personas();
+        $persona -> nombre = $request -> post("nombre");
+        $persona -> apellido = $request -> post("apellido");
+        $persona -> telefono = $request -> post("telefono");
+        $persona->save();
+    }
+}
